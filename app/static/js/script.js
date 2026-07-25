466982620db1c35b21dffbdbe25ba51612d25822
p47997au <anushay.usman@student.manchester.ac.uk>
2026-07-25 19:37:19 +0100

    display weather forecast details - daily

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -23,11 +23,32 @@ searchButton.addEventListener("click", async () => {
             <p>Feels like: ${hour.apparent_temperature}°C</p>
             <p>Precipitation probability: ${hour.precipitation_probability}%</p>
             <p>Weather code: ${hour.weather_code}</p>
-        `;
+        `;  
 
         hourlyForecastContainer.appendChild(hourCard);
     }
 
-    console.log(data);
+    const dailyForecastContainer =
+        document.getElementById("daily-forecast-container");
+
+    dailyForecastContainer.innerHTML = "";
+
+    for (let i = 0; i < data.daily_forecast.length; i++) {
+        const day = data.daily_forecast[i];
+
+        const dayCard = document.createElement("div");
+
+        dayCard.innerHTML = `
+            <p>Date: ${day.date}</p>
+            <p>Maximum temperature: ${day.max_temperature}°C</p>
+            <p>Mean temperature: ${day.mean_temperature}°C</p>
+            <p>Minimum temperature: ${day.min_temperature}°C</p>
+            <p>Weather code: ${day.weather_code}</p>
+        `;
+
+        dailyForecastContainer.appendChild(dayCard);
+    }
+
+    
 });
 
